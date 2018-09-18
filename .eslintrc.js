f1bb38b474d3969a02f200c6423272fc4db8ec58
chaunjie <894576801@qq.com>
2018-09-18 10:01:51 +0800

    #0: update eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,22 +8,16 @@ module.exports = {
     browser: true
   },
   globals: { wx: true },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
-  // required to lint *.wpy files
   plugins: [
     'html'
   ],
   settings: {
     'html/html-extensions': ['.html', '.xu']
   },
-  // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
     'generator-star-spacing': 0,
-    // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'space-before-function-paren': 0,
     'no-tabs': 'off',
